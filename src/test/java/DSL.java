692ec1882536a204113207e0601f9e42cc843f64
Jhonattan Vinicios De Oliveira Gome <jhonattangome@sisconsultoria.com.br>
2022-06-27 18:47:50 -0300

    Teste completo, com PageObjects e CleanCode

--- src/test/java/DSL.java
+++ src/test/java/DSL.java
@@ -1,2 +1,73 @@
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DSL {
+
+    private WebDriver driver;
+
+    public DSL(WebDriver driver) {
+        super();
+        this.driver = driver;
+    }
+
+    /********** Botao **********/
+
+    public void clicarBotao(String id) {
+        driver.findElement(By.id(id)).click();
+    }
+
+    /********** Alert **********/
+
+    public String alertaObterTextoEAceita() {
+        Alert alert = driver.switchTo().alert();
+        String valor = alert.getText();
+        alert.accept();
+        return valor;
+    }
+
+    /********** Texto **********/
+
+    public String obterTexto(By by) {
+        return driver.findElement(by).getText();
+    }
+
+    /********** Combo **********/
+
+    public int obterQuantidadeOpcoesCombo(String id) {
+        WebElement element = driver.findElement(By.id(id));
+        Select combo = new Select(element);
+        List<WebElement> options = combo.getOptions();
+        return options.size();
+    }
+
+    public void selecionarCombo(String id, String valor) {
+        WebElement element = driver.findElement(By.id(id));
+        Select combo = new Select(element);
+        combo.selectByVisibleText(valor);
+    }
+
+    public boolean verificarOpcaoCombo(String id, String opcao) {
+        WebElement element = driver.findElement(By.id(id));
+        Select combo = new Select(element);
+        List<WebElement> options = combo.getOptions();
+        for (WebElement option: options) {
+            if (option.getText().equals(opcao)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String obterValorCombo(String id) {
+        WebElement element = driver.findElement(By.id(id));
+        Select combo = new Select(element);
+        return combo.getFirstSelectedOption().getText();
+    }
+
 }
--- src/test/java/TesteCalculoUmidadeSolo.java
+++ src/test/java/TesteCalculoUmidadeSolo.java
@@ -1,2 +1,159 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class TesteCalculoUmidadeSolo {
+
+    private WebDriver driver;
+    private  DSL dsl;
+
+    @Before
+    public void inicializa() {
+        System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().setSize(new Dimension(1200, 765));
+        driver.get("https://calculoumidadesolo.hyggedigitaltec.repl.co/");
+        dsl = new DSL(driver);
+    }
+
+    @After
+    public void finaliza() {
+        driver.quit();
+    }
+
+    @Test
+    public void testeTextoCalculoDaUmidadeDoSolo() {
+        Assert.assertEquals("Cálculo da Umidade do Solo", dsl.obterTexto(By.tagName("h1")));
+    }
+
+    @Test
+    public void testeDeveVerificarValoresComboSoloFino() {
+        Assert.assertEquals(3, dsl.obterQuantidadeOpcoesCombo("umidadeSolo1"));
+    }
+
+    @Test
+    public void testeDeveVerificarValoresComboSoloMedio() {
+        Assert.assertEquals(3, dsl.obterQuantidadeOpcoesCombo("umidadeSolo2"));
+    }
+
+    @Test
+    public void testeDeveVerificarValoresComboSoloGrosseiro() {
+        Assert.assertEquals(3, dsl.obterQuantidadeOpcoesCombo("umidadeSolo3"));
+    }
+
+    @Test
+    public void testeDeveSelecionarValoresCombosSoloFino() {
+        Assert.assertTrue(dsl.verificarOpcaoCombo("umidadeSolo1", "Abaixo de 60"));
+        Assert.assertTrue(dsl.verificarOpcaoCombo("umidadeSolo1", "60 a 80"));
+        Assert.assertTrue(dsl.verificarOpcaoCombo("umidadeSolo1", "80 a 100"));
+    }
+
+    @Test
+    public void testeDeveSelecionarValoresCombosSoloMedio() {
+        Assert.assertTrue(dsl.verificarOpcaoCombo("umidadeSolo2", "Abaixo de 70"));
+        Assert.assertTrue(dsl.verificarOpcaoCombo("umidadeSolo2", "70 a 88"));
+        Assert.assertTrue(dsl.verificarOpcaoCombo("umidadeSolo2", "88 a 100"));
+    }
+
+    @Test
+    public void testeDeveSelecionarValoresCombosSoloGrosseiro() {
+        Assert.assertTrue(dsl.verificarOpcaoCombo("umidadeSolo3", "Abaixo de 80"));
+        Assert.assertTrue(dsl.verificarOpcaoCombo("umidadeSolo3", "80 a 90"));
+        Assert.assertTrue(dsl.verificarOpcaoCombo("umidadeSolo3", "90 a 100"));
+    }
+
+    @Test
+    public void testeAlertSoloFinoAbaixode60() {
+        dsl.selecionarCombo("umidadeSolo1", "Abaixo de 60");
+        Assert.assertEquals("Abaixo de 60", dsl.obterValorCombo("umidadeSolo1"));
+
+        dsl.clicarBotao("soloFino");
+        String texto = dsl.alertaObterTextoEAceita();
+        Assert.assertEquals("A umidade do solo está perigosamente baixa.\nA irrigação deve ser aplicada imediatamente.", texto);
+    }
+
+    @Test
+    public void testeAlertSoloFino60a80() {
+        dsl.selecionarCombo("umidadeSolo1", "60 a 80");
+        Assert.assertEquals("60 a 80", dsl.obterValorCombo("umidadeSolo1"));
+
+        dsl.clicarBotao("soloFino");
+        String texto = dsl.alertaObterTextoEAceita();
+        Assert.assertEquals("A umidade do solo está baixa.\nA irrigação deve ser aplicada.", texto);
+    }
+
+    @Test
+    public void testeAlertSoloFino80a100() {
+        dsl.selecionarCombo("umidadeSolo1", "80 a 100");
+        Assert.assertEquals("80 a 100", dsl.obterValorCombo("umidadeSolo1"));
+
+        dsl.clicarBotao("soloFino");
+        String texto = dsl.alertaObterTextoEAceita();
+        Assert.assertEquals("A umidade do solo está ideal.\nA irrigação não é necessária.", texto);
+    }
+
+    @Test
+    public void testeAlertSoloMedioAbaixode70() {
+        dsl.selecionarCombo("umidadeSolo2", "Abaixo de 70");
+        Assert.assertEquals("Abaixo de 70", dsl.obterValorCombo("umidadeSolo2"));
+
+        dsl.clicarBotao("soloMedio");
+        String texto = dsl.alertaObterTextoEAceita();
+        Assert.assertEquals("A umidade do solo está perigosamente baixa.\nA irrigação deve ser aplicada imediatamente.", texto);
+    }
+
+    @Test
+    public void testeAlertSoloMedio70a88() {
+        dsl.selecionarCombo("umidadeSolo2", "70 a 88");
+        Assert.assertEquals("70 a 88", dsl.obterValorCombo("umidadeSolo2"));
+
+        dsl.clicarBotao("soloMedio");
+        String texto = dsl.alertaObterTextoEAceita();
+        Assert.assertEquals("A umidade do solo está baixa.\nA irrigação deve ser aplicada.", texto);
+    }
+
+    @Test
+    public void testeAlertSoloMedio88a100() {
+        dsl.selecionarCombo("umidadeSolo2", "88 a 100");
+        Assert.assertEquals("88 a 100", dsl.obterValorCombo("umidadeSolo2"));
+
+        dsl.clicarBotao("soloMedio");
+        String texto = dsl.alertaObterTextoEAceita();
+        Assert.assertEquals("A umidade do solo está ideal.\nA irrigação não é necessária.", texto);
+    }
+
+    @Test
+    public void testeAlertSoloGrosseiroAbaixode80() {
+        dsl.selecionarCombo("umidadeSolo3", "Abaixo de 80");
+        Assert.assertEquals("Abaixo de 80", dsl.obterValorCombo("umidadeSolo3"));
+
+        dsl.clicarBotao("soloGrosseiro");
+        String texto = dsl.alertaObterTextoEAceita();
+        Assert.assertEquals("A umidade do solo está perigosamente baixa.\nA irrigação deve ser aplicada imediatamente.", texto);
+    }
+
+    @Test
+    public void testeAlertSoloGrosseiro80a90() {
+        dsl.selecionarCombo("umidadeSolo3", "80 a 90");
+        Assert.assertEquals("80 a 90", dsl.obterValorCombo("umidadeSolo3"));
+
+        dsl.clicarBotao("soloGrosseiro");
+        String texto = dsl.alertaObterTextoEAceita();
+        Assert.assertEquals("A umidade do solo está baixa.\nA irrigação deve ser aplicada.", texto);
+    }
+
+    @Test
+    public void testeAlertSoloGrosseiro90a100() {
+        dsl.selecionarCombo("umidadeSolo3", "90 a 100");
+        Assert.assertEquals("90 a 100", dsl.obterValorCombo("umidadeSolo3"));
+
+        dsl.clicarBotao("soloGrosseiro");
+        String texto = dsl.alertaObterTextoEAceita();
+        Assert.assertEquals("A umidade do solo está ideal.\nA irrigação não é necessária.", texto);
+    }
 }
